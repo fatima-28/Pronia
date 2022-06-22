462e0c0bc9c401b012b244dcdd032a622a687e9b
fatima-28 <2n6gddb@code.edu.az>
2022-06-22 04:11:48 -0700

    Crud working

---
+++ Pronia BackEnd/Pronia/Pronia/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pronia.DAL;
+using Pronia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pronia.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class CategoryController : Controller
+    {
+        private AppDbContext _context { get;  }
+        public IEnumerable<Category> categories;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+            categories = _context.Categories.Where(c =>!c.IsDeleted).ToList();
+        }
+        public IActionResult Index()
+        {
+            return View(categories);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id==null)
+            {
+                return BadRequest();
+
+            }
+            var categoryDb = _context.Categories.Where(c => !c.IsDeleted).FirstOrDefault(c => c.Id == id);
+            if (categoryDb==null)
+            {
+                return NotFound();
+            }
+
+            categoryDb.IsDeleted = true;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            bool IsExist = categories.Where(c => !c.IsDeleted).Any(c => c.Name.ToLower() == category.Name.ToLower());
+
+          
+        
+
+            if (IsExist)
+            {
+                ModelState.AddModelError("Name", $"{category.Name} is already exist");
+                return View();
+            }
+
+            Category newCategory = new Category
+            {
+
+                Name = category.Name
+
+            };
+            await _context.Categories.AddAsync(newCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Category category)
+        {
+            if (id == null)
+                return BadRequest();
+
+
+            Category categoryDb = _context.Categories.Where(c => !c.IsDeleted).FirstOrDefault(c => c.Id == id);
+            if (categoryDb == null)
+                return NotFound();
+
+
+            if (category.Name.ToLower() == categoryDb.Name.ToLower())
+                return RedirectToAction(nameof(Index));
+            bool IsExist = categories.Any(c => c.Name.ToLower() == category.Name.ToLower());
+            if (IsExist)
+            {
+                ModelState.AddModelError("Name", $"{category.Name} already  exist");
+                return View();
+            }
+
+            categoryDb.Name = category.Name;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Pronia BackEnd/Pronia/Pronia/obj/Debug/netcoreapp3.1/Razor/Areas/AdminPanel/Views/Dashboard/Index.cshtml.g.cs
+++ Pronia BackEnd/Pronia/Pronia/obj/Debug/netcoreapp3.1/Razor/Areas/AdminPanel/Views/Dashboard/Index.cshtml.g.cs
@@ -13,6 +13,7 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"83872c409543ed4b61aab2914e2bdaf7d37b1145", @"/Areas/AdminPanel/Views/Dashboard/Index.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"a9af4978b9c2bfca24ef48e96efe5f8573634464", @"/Areas/AdminPanel/Views/_ViewImports.cshtml")]
     public class Areas_AdminPanel_Views_Dashboard_Index : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
